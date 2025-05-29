9eec1268849cd008abadd073c9e13a13afb46504
IrfanMahmud <irfanmahmud201@gmail.com>
2025-05-29 23:43:32 +0600

    configure ApplicationDbContext

--- src/App.Qtech/App.Qtech.Infrastructure/ApplicationDbContext.cs
+++ src/App.Qtech/App.Qtech.Infrastructure/ApplicationDbContext.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
 
 namespace App.Qtech.Web.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+
+        private readonly string _connectionString;
+        private readonly string _migrationAssembly;
+        public ApplicationDbContext(string connectionString, string migrationAssembly)
         {
+            _connectionString = connectionString;
+            _migrationAssembly = migrationAssembly;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString, (x) => x.MigrationsAssembly(_migrationAssembly));
+            }
+            base.OnConfiguring(optionsBuilder);
         }
     }
 }
