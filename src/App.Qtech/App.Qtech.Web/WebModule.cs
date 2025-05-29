875ab5f916083ed89a58dffbc1ce0288d9d92070
IrfanMahmud <irfanmahmud201@gmail.com>
2025-05-29 23:16:10 +0600

    add WebModule class to register services

---
+++ src/App.Qtech/App.Qtech.Web/WebModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using System.ComponentModel;
+
+namespace App.Qtech.Web
+{
+    public class WebModule : Module
+    {
+        private readonly string _connectionString;
+        private readonly string _migrationAssembly;
+        public WebModule(string connectionString, string migrationAssembly)
+        {
+            _connectionString = connectionString;
+            _migrationAssembly = migrationAssembly;
+        }
+    }
+}
