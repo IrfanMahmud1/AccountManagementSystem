3f03b553eb56965aa9e762058c208e5785478ea1
IrfanMahmud <irfanmahmud201@gmail.com>
2025-05-31 09:01:22 +0600

    use try catch finally in program.cs

--- src/App.Qtech/App.Qtech.Web/Program.cs
+++ src/App.Qtech/App.Qtech.Web/Program.cs
@@ -9,52 +9,63 @@ using App.Qtech.Infrastructure.Data;
 using App.Qtech.Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity;
 
-var builder = WebApplication.CreateBuilder(args);
-var migrationAssembly = (typeof(ApplicationDbContext).GetTypeInfo().Assembly.GetName().Name ?? throw new InvalidOperationException("Migration assembly name not found."));
-// Add services to the container.
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-#region Identity Configuration
-builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
-#endregion
-#region Autofac Configuration
-builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+try
 {
-    containerBuilder.RegisterModule(new WebModule(connectionString, migrationAssembly));
-});
-#endregion
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+    var builder = WebApplication.CreateBuilder(args);
+    var migrationAssembly = (typeof(ApplicationDbContext).GetTypeInfo().Assembly.GetName().Name ?? throw new InvalidOperationException("Migration assembly name not found."));
+    // Add services to the container.
 
-builder.Services.AddRazorPages();
-builder.Services.AddAuthorization();
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    #region Identity Configuration
+    builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
+    #endregion
+    #region Autofac Configuration
+    builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+    builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+    {
+        containerBuilder.RegisterModule(new WebModule(connectionString, migrationAssembly));
+    });
+    #endregion
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString));
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-var app = builder.Build();
+    builder.Services.AddRazorPages();
+    builder.Services.AddAuthorization();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapStaticAssets();
+    app.MapRazorPages()
+       .WithStaticAssets();
+
+    app.Run();
 }
-else
+catch (Exception ex)
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+   
 }
+finally
+{
 
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
-
-app.Run();
+}
