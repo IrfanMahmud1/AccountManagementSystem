94aaf270af0b0c2d1b8012da40ea9f28ce725b3b
IrfanMahmud <irfanmahmud201@gmail.com>
2025-06-01 13:18:24 +0600

    add ChartOfAccount and IEntity entities

---
+++ src/App.Qtech/App.Qtech.Domain/Entities/ChartOfAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Qtech.Domain.Entities
+{
+    public class ChartOfAccount : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid? ParentId { get; set; }
+        public string AccountType { get; set; } // Asset, Liability, Income, etc.
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
