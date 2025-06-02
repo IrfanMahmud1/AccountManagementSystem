7fe7596a17f292ec36bbeb9ef7663f5386c0bfec
IrfanMahmud <irfanmahmud201@gmail.com>
2025-06-02 12:04:18 +0600

    add ChartOfAccountService with implementation of AddAccountAsync method

---
+++ src/App.Qtech/App.Qtech.Application/Services/ChartOfAccountService.cs
@@ -0,0 +1,25 @@
+﻿using App.Qtech.Domain.Entities;
+using App.Qtech.Domain.Repositories;
+using App.Qtech.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Qtech.Application.Services
+{
+    public class ChartOfAccountService : IChartOfAccountService
+    {
+        private readonly IChartOfAccountRepository _repository;
+
+        public ChartOfAccountService(IChartOfAccountRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task AddAccountAsync(ChartOfAccount account) => await _repository.AddAsync(account);
+
+    }
+
+}
