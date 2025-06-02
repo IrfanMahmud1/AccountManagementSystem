9dfa516d1265c183c579093d8974874c4a7d2fcb
IrfanMahmud <irfanmahmud201@gmail.com>
2025-06-02 14:17:27 +0600

    add razor page Index.cshtml with UI for Admin Dashboard and modify _AdminLayout Ui

---
+++ src/App.Qtech/App.Qtech.Web/Areas/Admin/Pages/Dashboard/Index.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace App.Qtech.Web.Areas.Admin.Pages.Dashboard
+{
+    [Authorize(Roles = "Admin")]
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+            _logger.LogInformation("Dashboard accessed at {Time}", DateTime.UtcNow);
+        }
+    }
+}
