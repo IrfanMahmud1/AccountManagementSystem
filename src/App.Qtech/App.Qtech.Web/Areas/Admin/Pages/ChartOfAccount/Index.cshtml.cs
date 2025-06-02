01bf2e24014937320d13e05da23c8506dcf0dd2f
IrfanMahmud <irfanmahmud201@gmail.com>
2025-06-02 19:38:12 +0600

    add Index razor page with its model

---
+++ src/App.Qtech/App.Qtech.Web/Areas/Admin/Pages/ChartOfAccount/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using App.Qtech.Domain.Entities;
+using App.Qtech.Infrastructure.Data;
+
+namespace App.Qtech.Web.Areas.Admin.Pages.ChartOfAccount
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<App.Qtech.Domain.Entities.ChartOfAccount> ChartOfAccount { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            ChartOfAccount = await _context.ChartOfAccounts.ToListAsync();
+        }
+    }
+}
