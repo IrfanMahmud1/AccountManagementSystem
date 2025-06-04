af11f74bf4a67803fe380fb6fbe2a224d51f12c0
IrfanMahmud <irfanmahmud201@gmail.com>
2025-06-04 11:46:26 +0600

    fix repository methods name in service

--- src/App.Qtech/App.Qtech.Application/Services/ChartOfAccountService.cs
+++ src/App.Qtech/App.Qtech.Application/Services/ChartOfAccountService.cs
@@ -20,8 +20,8 @@ namespace App.Qtech.Application.Services
 
         public async Task<List<ChartOfAccount>> GetAllAccountsAsync() => await _repository.GetAllAsync();
         public async Task AddAccountAsync(ChartOfAccount account) => await _repository.CreateAsync(account);
-        public async Task UpdateAccountAsync(ChartOfAccount account) => await _repository.CreateAsync(account);
-        public async Task DeleteAccountAsync(Guid id) => await _repository.CreateAsync(id);
+        public async Task UpdateAccountAsync(ChartOfAccount account) => await _repository.UpdateAsync(account);
+        public async Task DeleteAccountAsync(Guid id) => await _repository.DeleteAsync(id);
 
     }
 
