258e3f7badeb7a850da032ee9db787caf31b6ec0
IrfanMahmud <irfanmahmud201@gmail.com>
2025-06-04 11:07:52 +0600

    add and implement Create,Update,Delete methods in ChartOfAccount repository

--- src/App.Qtech/App.Qtech.Infrastructure/Repositories/ChartOfAccountRepository.cs
+++ src/App.Qtech/App.Qtech.Infrastructure/Repositories/ChartOfAccountRepository.cs
@@ -1,35 +1,159 @@
 ﻿using App.Qtech.Domain.Entities;
 using App.Qtech.Domain.Repositories;
 using App.Qtech.Infrastructure.Data;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace App.Qtech.Infrastructure.Repositories
 {
+    //public class ChartOfAccountRepository(IConfiguration configration) : IChartOfAccountRepository
+    //{
+    //    private readonly string _connection = configration.GetConnectionString("DefaultConnection");
+
+    //    public async Task<List<ChartOfAccount>> GetAllAsync()
+    //    {
+    //        var parameters = new Dictionary<string, object>
+    //        {
+    //            { "Action","SELECT" }
+    //        };
+    //        var command = CreateCommand("sp_ManageChartOfAccounts", parameters);
+    //        var reader = await command.ExecuteReaderAsync();
+    //        var result = new List<ChartOfAccount>();
+    //        while(await reader.ReadAsync())
+    //        {
+    //            for (int i = 0; i < reader.FieldCount; i++)
+    //            {
+    //                var account = new ChartOfAccount
+    //                {
+    //                    Id = reader.GetGuid(reader.GetOrdinal("Id")),
+    //                    Name = reader.GetString(reader.GetOrdinal("Name")),
+    //                    ParentId = reader.GetGuid(reader.GetOrdinal("ParentId")),
+    //                    AccountType = reader.GetString(reader.GetOrdinal("AccountType")),
+    //                    IsActive = reader.GetBoolean(reader.GetOrdinal("IsActive")),
+    //                    CreatedAt = reader.GetDateTime(reader.GetOrdinal("CreatedAt"))
+    //                };
+    //                result.Add(account);
+    //            }
+    //        }
+    //        return result;
+    //    }
+    //    public async Task AddAsync(ChartOfAccount account)
+    //    {
+
+    //    }
+    //    private DbCommand CreateCommand(string storedProcedureName,
+    //        IDictionary<string, object> parameters = null)
+    //    {
+    //        var command = new SqlCommand(_connection);
+    //        command.CommandText = storedProcedureName;
+    //        command.CommandType = CommandType.StoredProcedure;
+
+    //        if (parameters != null)
+    //        {
+    //            foreach (var item in parameters)
+    //            {
+    //                command.Parameters.Add(new SqlParameter(item.Key, item.Value));
+    //            }
+    //        }
+
+    //        return command;
+    //    }
+
+    //}
     public class ChartOfAccountRepository : IChartOfAccountRepository
     {
-        private readonly ApplicationDbContext _context;
+        private readonly string _connectionString;
 
-        public ChartOfAccountRepository(ApplicationDbContext context)
+        public ChartOfAccountRepository(IConfiguration configuration)
         {
-            _context = context;
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
         }
+
         public async Task<List<ChartOfAccount>> GetAllAsync()
         {
-            return await _context.ChartOfAccounts
-            .FromSqlRaw("EXEC sp_ManageChartOfAccounts @Action='SELECT'")
-            .ToListAsync();
-        }
-        public async Task AddAsync(ChartOfAccount account)
-        {
-            var sql = "EXEC sp_ManageChartOfAccounts @Action='INSERT', @Name={0}, @ParentId={1}, @AccountType={2}, @IsActive={3}";
-            await _context.Database.ExecuteSqlRawAsync(sql, account.Name, account.ParentId, account.AccountType, account.IsActive);
+            var accounts = new List<ChartOfAccount>();
+
+            using var connection = new SqlConnection(_connectionString);
+            using var command = new SqlCommand("sp_ManageChartOfAccounts", connection)
+            {
+                CommandType = CommandType.StoredProcedure,
+            };
+
+            command.Parameters.AddWithValue("@Action", ChartOfAccountAction.Get.ToString());
+            command.Parameters.AddWithValue("@Id", DBNull.Value);
+
+            await connection.OpenAsync();
+            using var reader = await command.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                accounts.Add(new ChartOfAccount
+                {
+                    Id = reader.GetGuid(reader.GetOrdinal("Id")),
+                    Name = reader.GetString(reader.GetOrdinal("Name")),
+                    ParentId = reader.IsDBNull(reader.GetOrdinal("ParentId")) ? null : reader.GetGuid(reader.GetOrdinal("ParentId")),
+                    AccountType = reader.GetString(reader.GetOrdinal("AccountType")),
+                    IsActive = reader.GetBoolean(reader.GetOrdinal("IsActive"))
+                });
+            }
+
+            return accounts;
         }
 
+        public async Task<bool> CreateAsync(ChartOfAccount account)
+        {
+            return await ExecuteNonQuery(account, ChartOfAccountAction.Create);
+        }
+
+        public async Task<bool> UpdateAsync(ChartOfAccount account)
+        {
+            return await ExecuteNonQuery(account, ChartOfAccountAction.Update);
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            var account = new ChartOfAccount { Id = id };
+            return await ExecuteNonQuery(account, ChartOfAccountAction.Delete);
+        }
+
+        private async Task<bool> ExecuteNonQuery(ChartOfAccount account, ChartOfAccountAction operation)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            using var command = new SqlCommand("sp_ManageChartOfAccounts", connection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            command.Parameters.AddWithValue("@Action", operation.ToString());
+            command.Parameters.AddWithValue("@Id", account.Id);
+
+            if (operation is ChartOfAccountAction.Create or ChartOfAccountAction.Update)
+            {
+                command.Parameters.AddWithValue("@Name", account.Name ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@ParentId", account.ParentId ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@AccountType", account.AccountType ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@IsActive", account.IsActive);
+            }
+
+            await connection.OpenAsync();
+            return await command.ExecuteNonQueryAsync() > 0;
+        }
+    }
+    public enum ChartOfAccountAction
+    {
+        Get,
+        Create,
+        Update,
+        Delete
     }
 
 }
---
+++ src/App.Qtech/App.Qtech.Web/Areas/Admin/Pages/ChartOfAccount/Create.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using App.Qtech.Domain.Entities;
+using App.Qtech.Infrastructure.Data;
+using App.Qtech.Domain.Services;
+
+namespace App.Qtech.Web.Areas.Admin.Pages.ChartOfAccount
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IChartOfAccountService _chartOfAccountService;
+        private readonly ILogger<CreateModel> _logger;
+
+        [BindProperty]
+        public App.Qtech.Domain.Entities.ChartOfAccount ChartOfAccount { get; set; } = default!;
+        public List<App.Qtech.Domain.Entities.ChartOfAccount> ParentAccounts { get; set; }
+        public CreateModel(IChartOfAccountService chartOfAccountService, ILogger<CreateModel> logger)
+        {
+            _chartOfAccountService = chartOfAccountService;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            ParentAccounts = await _chartOfAccountService.GetAllAccountsAsync();
+            return Page();
+        }
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _chartOfAccountService.AddAccountAsync(ChartOfAccount);
+
+                    return RedirectToPage("./Index");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogInformation(ex, "Faild to create account");
+                }
+            }
+            ParentAccounts = await _chartOfAccountService.GetAllAccountsAsync();
+            return Page();
+        }
+    }
+}
