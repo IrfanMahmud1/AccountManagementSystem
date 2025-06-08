c9c2bb673f3ea48db78f3b8cc78ed5d356576a78
IrfanMahmud <irfanmahmud201@gmail.com>
2025-06-08 23:42:56 +0600

    add IVoucherService interface and its implementation

---
+++ src/App.Qtech/App.Qtech.Application/Services/VoucherService.cs
@@ -0,0 +1,24 @@
+﻿using App.Qtech.Domain.Entities;
+using App.Qtech.Domain.Repositories;
+using App.Qtech.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Qtech.Application.Services
+{
+    public class VoucherService : IVoucherService
+    {
+        private readonly IVoucherRepository _voucherRepository;
+        public VoucherService(IVoucherRepository voucherRepository)
+        {
+            _voucherRepository = voucherRepository;
+        }
+        public Task SaveVoucherAsync(Voucher voucher)
+        {
+            return _voucherRepository.SaveAsync(voucher);
+        }
+    }
+}
