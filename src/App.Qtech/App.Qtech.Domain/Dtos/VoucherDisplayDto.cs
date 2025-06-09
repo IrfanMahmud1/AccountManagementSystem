58f4109e2bbc864182b3110816a5541aafe25b3c
IrfanMahmud <irfanmahmud201@gmail.com>
2025-06-09 20:33:44 +0600

    add VoucherDisplayDto

---
+++ src/App.Qtech/App.Qtech.Domain/Dtos/VoucherDisplayDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Qtech.Domain.Dtos
+{
+    public class VoucherDisplayDto
+    {
+        public DateTime Date { get; set; }
+        public string ReferenceNo { get; set; }
+        public string Type { get; set; }
+        public string AccountName { get; set; }
+        public decimal Debit { get; set; }
+        public decimal Credit { get; set; }
+    }
+
+}
