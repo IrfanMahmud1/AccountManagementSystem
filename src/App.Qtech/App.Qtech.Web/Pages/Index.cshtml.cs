28db3d00399bc2dbd2011a08eaa64099a1de7e93
IrfanMahmud <irfanmahmud201@gmail.com>
2025-06-11 22:19:25 +0600

    bind new property Modules in Index page model and add Ui to see accessible modules

--- src/App.Qtech/App.Qtech.Web/Pages/Index.cshtml.cs
+++ src/App.Qtech/App.Qtech.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,36 @@
+using App.Qtech.Domain.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace App.Qtech.Web.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IRoleModuleAccessService _roleModuleAccessService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        [BindProperty(SupportsGet = true)]
+        public List<string> Modules { get; set; } = new List<string>();
+
+        public IndexModel(ILogger<IndexModel> logger, IRoleModuleAccessService roleModuleAccessService)
         {
             _logger = logger;
+            _roleModuleAccessService = roleModuleAccessService;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
-
+            try
+            {
+                var role = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value;
+                Modules = await _roleModuleAccessService.GetAllModuleNamesByRoleNameAsync(role);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "Failed to retrieve modules for the user role.");
+            }
         }
     }
 }
