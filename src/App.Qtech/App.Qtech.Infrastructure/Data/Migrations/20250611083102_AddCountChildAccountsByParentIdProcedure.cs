62dd04a74b094e50b57695d0dc9ee34ee964df4e
IrfanMahmud <irfanmahmud201@gmail.com>
2025-06-11 14:43:22 +0600

    add new migration AddCountChildAccountsByParentIdProcedure

---
+++ src/App.Qtech/App.Qtech.Infrastructure/Data/Migrations/20250611083102_AddCountChildAccountsByParentIdProcedure.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Qtech.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCountChildAccountsByParentIdProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = """"
+                                CREATE OR ALTER PROCEDURE [dbo].[sp_countChildAccountsByParentId]
+                    @Id UNIQUEIDENTIFIER
+                AS
+                BEGIN
+                    SET NOCOUNT ON;
+
+                    SELECT COUNT(*) AS ChildCount
+                    FROM ChartOfAccounts
+                    WHERE ParentId = @Id;
+                END;
+                """";
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sql = "DROP PROCEDURE [dbo].[sp_countChildAccountsByParentId]";
+            migrationBuilder.Sql(sql);
+        }
+    }
+}
