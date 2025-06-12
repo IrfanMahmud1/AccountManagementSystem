00d75d8f4c0b46db6d4cada577da92e7ced56bba
IrfanMahmud <irfanmahmud201@gmail.com>
2025-06-12 10:36:37 +0600

    add PagedResult<T> object inside valueobjects folder

---
+++ src/App.Qtech/App.Qtech.Domain/ValueObjects/PagedResult.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+namespace App.Qtech.Domain.ValueObjects
+{
+    public class PagedResult<T>
+    {
+        public List<T> Items { get; set; } = new();
+        public int TotalItems { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public int TotalPages => (int)Math.Ceiling(TotalItems / (double)PageSize);
+    }
+}
+
